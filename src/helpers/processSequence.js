ff16c61f7be799f9171be8129f996bf2c615cd3b
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-20 01:42:50 +0600

    complete 2nd task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,84 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+  allPass,
+  compose,
+  prop,
+  test,
+  tap,
+  andThen,
+  modulo,
+  length,
+  __, otherwise, curry,
+} from 'ramda'
 
- const api = new Api();
+// валидация
+const regChars = /^[1-9]\d*\.?\d+$/
+const regLength = /^.{3,9}$/
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isOfValidChars = test(regChars)
+const isOfValidLength = test(regLength)
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const isValueValid = allPass([isOfValidChars, isOfValidLength])
+const validate = value => new Promise((resolve, reject) => {
+  return isValueValid(value) ? resolve(value) : reject('validationError')
+})
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+// обработка промиса
+const promise = curry((rejectHandler, resolveHandler, promise) => {
+  return compose(
+    otherwise(rejectHandler),
+    andThen(resolveHandler),
+    promise,
+  )
+})
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+// математические функции
+const round = (string) => Math.round(Number(string))
+const square = (number) => Math.pow(number, 2)
+const mod3 = modulo(__, 3)
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+// функции api
+const api = new Api();
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const convertNumberBase = api.get('https://api.tech/numbers/base')
+const getConvertedNumber = (number) => convertNumberBase({from: 2, to: 10, number: `${number}`})
+const getAnimal = (animalId) => api.get(`https://animals.tech/${animalId}`)({})
+const parseServerResponse = prop('result')
+
+// master функция
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const promiseWithRejection = promise(handleError)
+  const log = tap(writeLog)
+
+  const handleAnimal = compose(
+    tap(handleSuccess),
+    parseServerResponse,
+  )
+
+  const handleConvertedNumber = compose(
+    promiseWithRejection(handleAnimal, getAnimal),
+    log,
+    mod3,
+    log,
+    square,
+    log,
+    length,
+    parseServerResponse,
+  )
+
+  const handleValidatedInput = compose(
+    promiseWithRejection(handleConvertedNumber, getConvertedNumber),
+    log,
+    round,
+  )
+
+  compose(
+    promiseWithRejection(handleValidatedInput, validate),
+    log,
+  )(value)
+}
 
 export default processSequence;
